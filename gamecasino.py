7a2cd76237797d28c12958b3815a0138984ac092
Ekaterina4ub <muron_89@mail.ru>
2017-04-27 22:04:21 +0300

    Same as last commit with changes

--- gamecasino.py
+++ gamecasino.py
@@ -1,29 +1,22 @@
-#coding: utf-8
+# coding: utf-8
 import random
 
 attempt = 1
 
-welcome_message = raw_input("Hi! Do you want to play the game? ")
-
-def check_result():
-    global number
-    number = input("")
-    global reference_number
-    reference_number= random.randint(1, 10)
-    print(reference_number)
+welcome_message = input("Hi! Do you want to play the game? ")
 
 if welcome_message == "yes":
     print("Let's start! Enter number from 1 to 10: ")
-    check_result()
 
     while attempt < 10:
-
+        number = int(input())
+        reference_number = random.randint(1, 10)
+        print(reference_number)
         if number == reference_number:
             print("You win!")
             exit()
         else:
             print("Oops! Try again.")
-            check_result()
             attempt += 1
 else:
     print("Goodbye!")
